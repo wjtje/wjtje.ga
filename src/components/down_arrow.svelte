9dc9d02c07456a890cb2b43862e6e22f27db77c8
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2021-04-18 23:25:15 +0200

    🚧 Added down arrow and some temp content

---
+++ src/components/down_arrow.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { fly } from "svelte/transition";
+
+	function handleClick() {
+		// Scroll to the main content
+		window.scrollTo({
+			behavior: "smooth",
+			top: window.innerHeight,
+		});
+	}
+</script>
+
+<svg
+	viewBox="0 0 80 46"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	on:click={handleClick}
+	transition:fly={{ y: -100, duration: 1000, delay: 2000 }}
+>
+	<path
+		d="M5.5 6L40 40L74.5 6"
+		stroke="white"
+		stroke-width="11"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
+
+<style>
+	svg {
+		width: 2rem;
+		position: absolute;
+		display: block;
+		top: 92vh;
+		cursor: pointer;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,12 @@
 <script>
-	import Logo from "../components/Logo.svelte";
+	import Logo from "../components/logo.svelte";
 	import { onMount } from "svelte";
+	import DownArrow from "../components/down_arrow.svelte";
 
-	let showLogo = false;
+	let ready = false;
 
 	onMount(() => {
-		showLogo = true;
+		ready = true;
 	});
 </script>
 
@@ -17,18 +18,60 @@
 	/>
 </svelte:head>
 
-<main>
-	{#if showLogo}
+<header>
+	{#if ready}
 		<Logo />
+
+		<DownArrow />
 	{/if}
+</header>
+
+<main>
+	<h1>This site is still work in progress</h1>
+
+	<article>
+		<h2>Projects</h2>
+		<ul>
+			<li>Trackless</li>
+			<li>Trackless for flutter</li>
+			<li>2048 for the Casio FX-CG50</li>
+			<li>wjtje.ga</li>
+			<li>Flutter skeleton animation</li>
+		</ul>
+	</article>
+
+	<article>
+		<h2>About me</h2>
+		<p>
+			An 18 year old developer that is experimenting with Typescript, React,
+			Flutter and Svelte.
+		</p>
+		<ul>
+			<li>Twitter</li>
+			<li>Github</li>
+			<li>Discord</li>
+			<li>Telegram</li>
+		</ul>
+	</article>
 </main>
 
 <style>
-	main {
+	* {
+		box-sizing: border-box;
+		font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+	}
+
+	header {
 		width: 100%;
 		height: 100vh;
 		display: flex;
 		align-items: center;
 		justify-content: center;
+	}
+
+	main {
+		width: 100%;
+		min-height: 100vh;
+		padding: 0 2rem;
 	}
 </style>
